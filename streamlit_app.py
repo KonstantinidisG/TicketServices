530e4a36cf1be0aaee151f344ac7c78659f058ac
KonstantinidisG <giorgos_koni@icloud.com>
2024-11-10 19:21:19 +0000

    tseting

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,3 @@
-from asyncio import sleep
-from backend.classes import Check
-from frontend.components.allert_components import allert_components
 import streamlit as st
 
 st.write("hello")
-status=Check()
-value=status.kalimera()
-if value:
-    st.write("allaksa")
-
-sleep(3)
-value=status.kalinixta()
-if not value:
-    modal=allert_components()
-    modal.hello_world()
