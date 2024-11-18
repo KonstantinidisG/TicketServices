fc524f25b4474e9a83e92fcf070d0b0e1474f508
KonstantinidisG <giorgos_koni@icloud.com>
2024-11-18 19:03:16 +0200

    testing

--- backend/classes.py
+++
@@ -1,7 +0,0 @@
-class Check:
-    def kalimera(self):
-        return True
-
-    def kalinixta(self):
-        return False
-        
--- frontend/components/allert_components.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-class allert_components: 
-    @st.dialog("modal")
-    def hello_world(self):
-        st.write("eisai malakas")
